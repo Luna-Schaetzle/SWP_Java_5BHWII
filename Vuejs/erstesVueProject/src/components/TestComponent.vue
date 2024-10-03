1d1b390c22d33572c94d2e6aab9f6c39eead4012
Luna-Schaetzle <luna.schaetzle@gmail.com>
2024-10-03 13:31:46 +0200

    vue.js start eigenes Programm | dateien verschoben

---
+++ Vuejs/erstesVueProject/src/components/TestComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  Property: string
+  counter: number
+}>()
+const Text = 'const Text'
+</script>
+
+<template>
+  <h1>Test Component</h1>
+  <h2>{{ Text }}</h2>
+  <h3>{{ Property }}</h3>
+  <h2>{{ counter }}</h2>
+</template>
---
+++ Vuejs/erstesVueProject/src/views/MyTestView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TestComponent from '../components/TestComponent.vue'
+let counter = ref(0)
+</script>
+
+<template>
+  <main>
+    <h1>Test</h1>
+    <!--Hier sollte eine Komponente sein-->
+    <test-component Property="Property Test" :counter="counter" />
+
+    <button @click="counter++">Increment</button>
+    <button @click="counter--">Decrement</button>
+  </main>
+</template>
